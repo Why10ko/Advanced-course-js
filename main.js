59f5d6caa749449e2815b1a3777bb3acb62e8aed
Yurii Voytenko <why10ko@yandex.ru>
2022-08-23 16:51:26 +0300

    Adding the method of the amount of goods. Adding a bucket class.

--- main.js
+++ main.js
@@ -1,23 +1,92 @@
-const product = [
-  {id:1,title:'Notebook',price:200000},
-  {id:2,title:'Mouse',price:2000},
-  {id:3,title:'Keybord',price:4000},
-  {id:4,title:'Phone',price:15000}
-];
-
-// В функции renderPage передать item, в функции renderProduct деструктурируем и задаем значения по умолчанию.
-
-const renderProduct = ({title="",price=0}) => 
-  `<div class="product-cell">
-    <h2>${title}</h2>
-    <p>${price}</p>
-    <button>Купить</button>
-  </div>`
-
-
-const renderPage = list => { 
-  // массив преобразовывается в строковой тип вместе с запятыми, метод join('') с пустой стройго помогает решить данную проблему. 
-  document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');;
+class ProductList {
+  constructor(container='.products'){
+    this.container = container;
+    this.goods = [];
+    this._fetchProducts();
+    this.render();
+    this.totalSum();
+  }
+  _fetchProducts(){
+    this.goods = [
+      {id:1,title:'Notebook',price:200000},
+      {id:2,title:'Mouse',price:2000},
+      {id:3,title:'Keybord',price:4000},
+      {id:4,title:'Phone',price:15000}
+    ];
+  }
+  render(){
+    const block = document.querySelector(this.container);
+    for(let product of this.goods){
+      const item = new ProductItem(product);
+      block.insertAdjacentHTML("beforeend",item.render());
+    }
+  }
+  totalSum(){
+    return this.goods.reduce((firstArg, {price}) => {
+      return firstArg + price;
+    },0)
+  }
 }
 
-renderPage(product);
+class ProductItem{
+  constructor(product){
+    this.title = product.title;
+    this.id = product.id;
+    this.price = product.price;
+  }
+  render(){
+    return `<div class="product-cell">
+    <h2>${this.title}</h2>
+    <p>${this.price}</p>
+    <button>Купить</button>
+  </div>`
+  }
+}
+
+let list = new ProductList();
+
+// Выводим сумму ксех товаров:
+let total = list.totalSum();
+console.log(total);
+
+class Basket {
+  addGoo () {
+
+  }
+  removeGood () {
+
+  }
+  changeGood() {
+
+  }
+  render() {
+
+  }
+ }
+
+ class ElemBasket {
+  render() {
+    
+  }
+ }
+
+// const product = [
+//   {id:1,title:'Notebook',price:200000},
+//   {id:2,title:'Mouse',price:2000},
+//   {id:3,title:'Keybord',price:4000},
+//   {id:4,title:'Phone',price:15000}
+// ];
+
+// const renderProduct = ({title="",price=0}) => 
+//   `<div class="product-cell">
+//     <h2>${title}</h2>
+//     <p>${price}</p>
+//     <button>Купить</button>
+//   </div>`
+
+
+// const renderPage = list => {  
+//   document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');;
+// }
+
+// renderPage(product);
